b301a53f40f9e21ce9ef66bb1f79b494e0cc06f4
Mats Egnell <mattegol.egnell@gmail.com>
2018-07-02 11:10:24 +0200

    Add Product Listing

--- Shopigol.WebUI/Controllers/HomeController.cs
+++ Shopigol.WebUI/Controllers/HomeController.cs
@@ -1,18 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopigol.Core.Contracts;
+using Shopigol.Core.Models;
+using Shopigol.WebUI.Models;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Shopigol.WebUI.Models;
 
 namespace Shopigol.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository<Product> _productRepository;
+        private readonly IRepository<ProductCategory> _productCategoryRepository;
+
+        public HomeController(IRepository<Product> productRepository,
+            IRepository<ProductCategory> productCategoryRepository)
+        {
+            _productRepository = productRepository;
+            _productCategoryRepository = productCategoryRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _productRepository.Collection().ToList();
+            return View(products);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var product = _productRepository.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
 
         public IActionResult About()
