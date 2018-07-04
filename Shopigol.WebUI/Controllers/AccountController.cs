8887c411c1b74bbba4e81c36d6290e88f096e4b6
Mats Egnell <mattegol.egnell@gmail.com>
2018-07-04 10:23:26 +0200

    Add new Bootstrap theme

--- Shopigol.WebUI/Controllers/AccountController.cs
+++ Shopigol.WebUI/Controllers/AccountController.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Shopigol.WebUI.Models;
 using Shopigol.WebUI.Models.AccountViewModels;
 using Shopigol.WebUI.Services;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Shopigol.WebUI.Controllers
 {
@@ -220,7 +216,11 @@ namespace Shopigol.WebUI.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
